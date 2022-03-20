1830593af8357d5e0986798f771b3f945cdbfe0d
unknown <connorking002@gmail.com>
2022-03-19 21:39:38 -0400

    LogEntry Done

--- A2/src/main/java/com/example/rest/Core/RepException.java
+++ A2/src/main/java/com/example/rest/Core/RepException.java
@@ -1,11 +1,24 @@
 package com.example.rest.Core;
 
-public class RepException extends Throwable{
+import javax.xml.ws.WebFault;
+import java.lang.Exception;
+
+@WebFault(name="RepException", targetNamespace = "http://localhost:8080/A2")
+public class RepException extends Exception{
     public RepException(String msg) {
         super(msg);
+        System.out.println(msg);
     }
 /*
     public void output() {
        return null;
     }*/
 }
+
+// THIS IS THE OLD RepException Code
+/*
+public class RepException extends Throwable{
+    public RepException(String msg) {
+        super(msg);
+    }
+ */
